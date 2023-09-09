4fefc9bdca44aab8559fe6361970a400ee6dd3f0
text-adi <alosha123456789049@gmail.com>
2023-09-10 02:12:07 +0300

    Added load env

--- app/main.py
+++ app/main.py
@@ -1,17 +1,28 @@
 """
 Файл налаштування проєкту
 """
+import os
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.handlers import v1
 from app.handlers.exception import error_4xx
 
-app = FastAPI()
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.path.exists('%s/.env' % BASE_DIR):
+    load_dotenv('%s/.env' % BASE_DIR)
+env = os.environ
 
 origins = [
-    "http://localhost:8080",
+    *env['ALLOWED_HOSTS'].split(',')
 ]
 basic_prefix = "/api"
+
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
